cb5c0f85733525a74cd69a276c8c7ae0bfc67657
kaceykooGithub <kacey.koo@gmail.com>
2025-03-05 15:54:05 +0000

    attempted fix

--- js/fetch_json.js
+++ js/fetch_json.js
@@ -1,7 +1,7 @@
 // Get JSON file and parse
 function jsonFromFile() {
      var request = new XMLHttpRequest();
-     request.open("GET", `../news.json`, false);
+     request.open("GET", `news.json`, false);
      request.send(null)
      return JSON.parse(request.responseText);
 }
