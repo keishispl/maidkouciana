a02033b6e25547202dfd2787c5b0df173267c2b3
kaceykooGithub <kacey.koo@gmail.com>
2025-04-21 18:07:06 +0900

    json folder

--- js/fetch_json.js
+++ js/fetch_json.js
@@ -2,9 +2,9 @@
 function jsonFromFile(path) {
      var request = new XMLHttpRequest();
      if (window.location.pathname.startsWith("/maidkouciana")) {
-          request.open("GET", `${window.location.origin}/maidkouciana/${path}.json`, false);
+          request.open("GET", `${window.location.origin}/maidkouciana/data/${path}.json`, false);
      } else {
-          request.open("GET", `${window.location.origin}/${path}.json`, false);
+          request.open("GET", `${window.location.origin}/data/${path}.json`, false);
      }
      request.send(null)
      return JSON.parse(request.responseText);
